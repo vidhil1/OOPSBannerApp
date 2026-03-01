ba22170bd7781f1a20f37f645e187f2273c1ed5e
vidhil1 <vv7559@srmist.edu.in>
2026-03-02 00:17:06 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,12 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
         // UC3: Using String.join("", ...) instead of + for better memory efficiency
-        System.out.println(String.join("", "  *** ", "  *** ", " ***** ", "  **** "));
-        System.out.println(String.join("", " ** ** ", " ** ** ", " ** ** ", " ** "));
-        System.out.println(String.join("", " ** ** ", " ** ** ", " ** ** ", " ** "));
-        System.out.println(String.join("", " ** ** ", " ** ** ", " ***** ", "  **** "));
-        System.out.println(String.join("", " ** ** ", " ** ** ", " ** ", "     **"));
-        System.out.println(String.join("", " ** ** ", " ** ** ", " ** ", "     **"));
-        System.out.println(String.join("", "  *** ", "  *** ", " ** ", "  **** "));
+        System.out.println(String.join("", "  *** ", "  *** ", "   ***** ", "  **** "));
+        System.out.println(String.join("", " **  ** ","**  ** ", " **  ** ", " ** "));
+        System.out.println(String.join("", " **  ** ","**  ** ", " **  ** ", " ** "));
+        System.out.println(String.join("", " **  ** ","**  ** ", " ***** ", "   **** "));
+        System.out.println(String.join("", " **  ** ","**  ** ", " ** ", "     **"));
+        System.out.println(String.join("", " **  ** ","**  ** ", " ** ", "     **"));
+        System.out.println(String.join("", "  *** ", "  *** ",   " ** ", "  **** "));
     }
 }
