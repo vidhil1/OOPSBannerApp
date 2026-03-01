45543bf5bc70518d37d08b24a94632140ea8e678
vidhil1 <vv7559@srmist.edu.in>
2026-03-02 00:02:12 +0530

    Fixed UC4 alignment and added spacing

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,25 @@
+/**
+ * OOPSBannerApp UC4: String Array and Loop
+ * @author Developer
+ * @version 4.0
+ */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        // UC3: Using String.join("", ...) instead of + for better memory efficiency
-        System.out.println(String.join("", "  *** ", "  *** ", " ***** ", "  **** "));
-        System.out.println(String.join("", " ** ** ", " ** ** ", " ** ** ", " ** "));
-        System.out.println(String.join("", " ** ** ", " ** ** ", " ** ** ", " ** "));
-        System.out.println(String.join("", " ** ** ", " ** ** ", " ***** ", "  **** "));
-        System.out.println(String.join("", " ** ** ", " ** ** ", " ** ", "     **"));
-        System.out.println(String.join("", " ** ** ", " ** ** ", " ** ", "     **"));
-        System.out.println(String.join("", "  *** ", "  *** ", " ** ", "  **** "));
+        // We use an array to store the 7 rows of the banner.
+        // I added extra "      " (6 spaces) between letters to prevent congestion.
+        String[] banner = {
+            String.join("      ", "  **** ", "  **** ", " ***** ", "  **** "),
+            String.join("      ", " ** ** ", " ** ** ", " ** ** ", " ** "),
+            String.join("      ", " ** ** ", " ** ** ", " ** ** ", " ** "),
+            String.join("      ", " ** ** ", " ** ** ", " ***** ", "  **** "),
+            String.join("      ", " ** ** ", " ** ** ", " ** ", "     ** "),
+            String.join("      ", " ** ** ", " ** ** ", " ** ", "     ** "),
+            String.join("      ", "  **** ", "  **** ", " ** ", "  **** ")
+        };
+
+        // This loop prints the array row by row
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
