961fbee8b587a1a28cf908dd7d2e73af7846afc9
vidhil1 <vv7559@srmist.edu.in>
2026-03-02 00:11:29 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,20 +1,14 @@
-/**
- * OOPSBannerApp UC4: String Array and Loop
- * @author Developer
- * @version 4.0
- */
 public class OOPSBannerApp {
     public static void main(String[] args) {
         // We use an array to store the 7 rows of the banner.
-        // I added extra "      " (6 spaces) between letters to prevent congestion.
         String[] banner = {
-            String.join("      ", "  **** ", "  **** ", " ***** ", "  **** "),
-            String.join("      ", " ** ** ", " ** ** ", " ** ** ", " ** "),
-            String.join("      ", " ** ** ", " ** ** ", " ** ** ", " ** "),
-            String.join("      ", " ** ** ", " ** ** ", " ***** ", "  **** "),
-            String.join("      ", " ** ** ", " ** ** ", " ** ", "     ** "),
-            String.join("      ", " ** ** ", " ** ** ", " ** ", "     ** "),
-            String.join("      ", "  **** ", "  **** ", " ** ", "  **** ")
+            String.join("      ", "  **** ", "  **** ", "   ***** ", "  **** "),
+            String.join("      ", " **   ** ","**   ** ", " **  ** ", " ** "),
+            String.join("      ", " **   ** ","**   ** ", " **  ** ", " ** "),
+            String.join("      ", " **   ** ","**   ** ", " ***** ", "   **** "),
+            String.join("      ", " **   ** ","**   ** ", " ** ", "     ** "),
+            String.join("      ", " **   ** ","**   ** ", " ** ", "     ** "),
+            String.join("      ", "  **** ", "  **** ",   " ** ", "  **** ")
         };
 
         // This loop prints the array row by row
