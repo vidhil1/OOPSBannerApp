a1abbf5d065f31773b622b8fef79abb409580645
vidhil1 <vv7559@srmist.edu.in>
2026-03-02 20:44:29 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,6 @@
-/**
- * OOPSBannerApp UC5: Inline Array Initialization
- * Combining array declaration and population in one step.
- * @author Developer
- * @version 5.0
- */
 public class OOPSBannerApp {
     public static void main(String[] args) {
         // UC5: Define and populate the array at the same time (Inline)
-        // Spacing is increased ("      ") to make the banner look big and clear
         String[] lines = {
             String.join("      ", "  **** ", "  **** ", " ***** ", "  **** "),
             String.join("      ", " **  **", " **  **", " **  **", " ** "),
