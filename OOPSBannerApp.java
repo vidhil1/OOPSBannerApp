505852fa535f7c366b401c158acf000973562fd5
vidhil1 <vv7559@srmist.edu.in>
2026-03-02 20:52:12 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,7 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
         // We use an array to store the 7 rows of the banner.
+    
         String[] banner = {
             String.join("      ", "  **** ", "  **** ", "   ***** ", "  **** "),
             String.join("      ", " **   ** ","**   ** ", " **  ** ", " ** "),
